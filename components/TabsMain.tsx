ec2b2c4a2aad7b04f45a2536f691b07aa7754116
PskCluB <passakon_p@hotmail.com>
2024-05-16 09:24:28 +0700

    feat: ui

--- components/TabsMain.tsx
+++ components/TabsMain.tsx
@@ -7,6 +7,11 @@ import { Swap } from '@/features/Swap'
 import { AddLiquidity } from '@/features/AddLiquidity'
 import { RemoveLiquidity } from '@/features/RemoveLiquidity'
 
+const defaultClassName =
+  'bg-background text-gray-400 border-1.5 border-[#242438] hover:bg-primary/10 hover:text-[#BD6CFF]'
+
+const activeClassName = 'bg-primary/10 text-[#BD6CFF]'
+
 export const TabsMain = () => {
   const [activeTab, setActiveTab] = React.useState('swap')
 
@@ -20,10 +25,9 @@ export const TabsMain = () => {
           radius={'full'}
           color="primary"
           size={'lg'}
-          className={classNames('font-medium', {
-            'bg-primary/10 text-[#BD6CFF]': activeTab === 'swap',
-            'bg-background text-gray-400 border-1.5 border-[#242438] hover:bg-primary/10 hover:text-[#BD6CFF]':
-              activeTab !== 'swap',
+          className={classNames('font-medium px-5', {
+            [activeClassName]: activeTab === 'swap',
+            [defaultClassName]: activeTab !== 'swap',
           })}
         >
           Swap
@@ -35,13 +39,13 @@ export const TabsMain = () => {
           radius={'full'}
           color="primary"
           size={'lg'}
-          className={classNames('font-medium', {
-            'bg-primary/10 text-[#BD6CFF]': activeTab === 'add',
-            'bg-background text-gray-400 border-1.5 border-[#242438] hover:bg-primary/10 hover:text-[#BD6CFF]':
-              activeTab !== 'add',
+          className={classNames('font-medium px-5', {
+            [activeClassName]: activeTab === 'add',
+            [defaultClassName]: activeTab !== 'add',
           })}
         >
-          Add<span className={'hidden md:flex'}>Liquidity</span>
+          Add
+          <span className={'hidden md:flex'}>Liquidity</span>
           <span className={'md:hidden'}>Liq.</span>
         </Button>
         <Button
@@ -51,13 +55,13 @@ export const TabsMain = () => {
           radius={'full'}
           color="primary"
           size={'lg'}
-          className={classNames('font-medium', {
-            'bg-primary/10 text-[#BD6CFF]': activeTab === 'remove',
-            'bg-background text-gray-400 border-1.5 border-[#242438] hover:bg-primary/10 hover:text-[#BD6CFF]':
-              activeTab !== 'remove',
+          className={classNames('font-medium px-5', {
+            [activeClassName]: activeTab === 'remove',
+            [defaultClassName]: activeTab !== 'remove',
           })}
         >
-          Remove<span className={'hidden  md:flex'}>Liquidity</span>
+          Remove
+          <span className={'hidden  md:flex'}>Liquidity</span>
           <span className={'md:hidden'}>Liq.</span>
         </Button>
       </div>
